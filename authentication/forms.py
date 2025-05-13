e96f2cc90bbb5eeeefb06c43ac4aa910777b4c05
axelle <kadance9720@gmail.com>
2025-05-13 15:22:07 +0200

    Delete auth forms.py file

--- authentication/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=50, label='Identifiant')
-    password = forms.CharField(max_length=50, label='Mot de passe', widget=forms.PasswordInput)
