46aca9066ab1a50126ef0597ef8593905cfba8c5
axelle <kadance9720@gmail.com>
2025-05-13 15:20:22 +0200

    Use login generic view from django and delete loginPage class

--- authentication/views.py
+++ authentication/views.py
@@ -1,31 +1,5 @@
-from django.contrib.auth import login, authenticate, logout
-from django.shortcuts import render, redirect
-from django.views.generic import View
-from . import forms
-
-class LoginPage(View):
-    form_class = forms.LoginForm
-    template_name = 'authentication/login.html'
-
-    def get(self, request):
-        form = self.form_class
-        message = ''
-        return render(request, self.template_name, context={'form' : form, 'message' : message})
-    
-    def post(self, request):
-        if request.method == 'POST':
-            form = self.form_class(request.POST)
-            if form.is_valid():
-                user = authenticate(
-                username = form.cleaned_data['username'],
-                password = form.cleaned_data['password']
-            )
-
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-        message = 'Identifiant invalides.'
-        return render(request, self.template_name, context={'form' : form, 'message' : message})
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
 def logout_user(request):
     logout(request)
