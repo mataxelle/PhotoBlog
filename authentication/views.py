2f03d2ce0d9d66f0de703ab0a70c81d24a54ee0c
axelle <kadance9720@gmail.com>
2025-05-16 17:16:54 +0200

    Create signup page

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,19 @@
-from django.contrib.auth import logout
-from django.shortcuts import redirect
+from django.conf import settings
+from django.contrib.auth import login, logout
+from django.shortcuts import redirect, render
+
+from . import forms
+
+def signup_page(request):
+    form = forms.SignupForm()
+
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request, 'authentication/signup.html', context={'form' : form})
 
 def logout_user(request):
     logout(request)
--- photoBlog/urls.py
+++ photoBlog/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth.views import (LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView)
 from django.urls import path
+import authentication.views
 import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', authentication.views.signup_page, name='signup'),
     path('', LoginView.as_view(
         template_name = 'authentication/login.html',
         redirect_authenticated_user = True
