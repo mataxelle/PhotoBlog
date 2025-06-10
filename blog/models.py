7a7a3b2c4fbcbf77bbc3cbf99c2f30a63855a96b
axelle <kadance9720@gmail.com>
2025-06-10 17:35:48 +0200

    Add personalized  image sizing method in Photo model with pillow

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,24 @@
 from django.conf import settings
 from django.db import models
 
+from PIL import Image
+
 class Photo(models.Model):
     image = models.ImageField()
     caption = models.CharField(max_length=150, blank=True)
     uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    IMAGE_MAX_SIZE = (800,800)
+
+    def resize_image(self):
+        image = Image.open(self.image)
+        image.thumbnail(self.IMAGE_MAX_SIZE)
+        image.save(self.image.path)
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self().resize_image()
 
 class Blog(models.Model):
     Photo = models.ForeignKey(Photo, null=True, on_delete=models.SET_NULL, blank=True)
