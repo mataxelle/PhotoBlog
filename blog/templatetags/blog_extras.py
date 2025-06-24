050c55e3401b234d816659d293bbf848da50299a
axelle <kadance9720@gmail.com>
2025-06-24 12:54:40 +0200

    Create personalised user name tag

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -5,3 +5,9 @@ register = Library()
 @register.filter
 def model_type(value):
     return type(value).__name__
+
+@register.simple_tag(takes_context=True)
+def get_poster_display(context, user):
+    if user == context['user']:
+        return 'vous'
+    return user.username
