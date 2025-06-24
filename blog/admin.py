9ce20db90bff5530eb41bbb1412fb55e35dd8ca5
axelle <kadance9720@gmail.com>
2025-06-24 12:52:41 +0200

    Upadate admin interface

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,16 @@ from django.contrib.auth.admin import UserAdmin
 from authentication.models import User
 from blog.models import Photo, Blog, BlogContributor
 
+class CustomUser(UserAdmin):
+    list_display = ('username', 'role')
+
+    # Ajoute les champs personnalisés dans la page de modification
+    fieldsets = UserAdmin.fieldsets + (
+        ('Informations supplémentaires', {
+            'fields': ('role', 'follows'),
+        }),
+    )
+
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ('caption', 'date_created', 'uploader')
 
@@ -12,7 +22,7 @@ class BlogAdmin(admin.ModelAdmin):
 class BlogContributorAdmin(admin.ModelAdmin):
     list_display = ('blog', 'contributor', 'contribution')
 
-admin.site.register(User, UserAdmin)
+admin.site.register(User, CustomUser)
 admin.site.register(Photo, PhotoAdmin)
 admin.site.register(Blog, BlogAdmin)
 admin.site.register(BlogContributor, BlogContributorAdmin)
